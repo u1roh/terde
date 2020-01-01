be78fa55d4d00ae0f0f5f503e13a43089c2156e5
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-01-01 15:00:22 +0900

    ReadPrimitive trait, Read trait

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,19 @@ mod code {
     pub const I32: u8 = 4;
 }
 
-pub type Result<T> = std::io::Result<T>;
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    TypeMismatch,
+}
+
+impl std::convert::From<std::io::Error> for Error {
+    fn from(src: std::io::Error) -> Self {
+        Error::IoError(src)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub trait WritePrimitive {
     fn u8(&mut self, x: u8) -> Result<()>;
@@ -14,8 +26,18 @@ pub trait WritePrimitive {
     fn i32(&mut self, x: i32) -> Result<()>;
 }
 
+pub trait ReadPrimitive {
+    fn u8(&mut self) -> Result<u8>;
+    fn u16(&mut self) -> Result<u16>;
+    fn i32(&mut self) -> Result<i32>;
+}
+
 pub trait Write: WritePrimitive {
     fn obj<S: Serialize>(&mut self, x: &S) -> Result<()>;
+}
+
+pub trait Read: ReadPrimitive {
+    fn obj<S: Deserialize>(&mut self) -> Result<S>;
 }
 
 impl<W: std::io::Write> WritePrimitive for W {
@@ -36,6 +58,24 @@ impl<W: std::io::Write> WritePrimitive for W {
             self.write(&std::mem::transmute::<i32, [u8; 4]>(x))?;
         }
         Ok(())
+    }
+}
+
+impl<R: std::io::Read> ReadPrimitive for R {
+    fn u8(&mut self) -> Result<u8> {
+        let mut buf = [0u8; 1];
+        self.read(&mut buf)?;
+        Ok(unsafe { std::mem::transmute(buf) })
+    }
+    fn u16(&mut self) -> Result<u16> {
+        let mut buf = [0u8; 2];
+        self.read(&mut buf)?;
+        Ok(unsafe { std::mem::transmute(buf) })
+    }
+    fn i32(&mut self) -> Result<i32> {
+        let mut buf = [0u8; 4];
+        self.read(&mut buf)?;
+        Ok(unsafe { std::mem::transmute(buf) })
     }
 }
 
@@ -68,9 +108,39 @@ impl<W: std::io::Write> Write for Writer<W> {
     }
 }
 
+struct Reader<R: std::io::Read>(R);
+
+impl<R: std::io::Read> ReadPrimitive for Reader<R> {
+    fn u8(&mut self) -> Result<u8> {
+        if self.0.u8()? == code::U8 {
+            self.0.u8()
+        } else {
+            Err(Error::TypeMismatch)
+        }
+    }
+    fn u16(&mut self) -> Result<u16> {
+        if self.0.u8()? == code::U16 {
+            self.0.u16()
+        } else {
+            Err(Error::TypeMismatch)
+        }
+    }
+    fn i32(&mut self) -> Result<i32> {
+        if self.0.u8()? == code::I32 {
+            self.0.i32()
+        } else {
+            Err(Error::TypeMismatch)
+        }
+    }
+}
+
 pub trait Serialize {
     const VERSION: u16 = 0;
     fn serialize(&self, write: &mut impl Write) -> Result<()>;
+}
+
+pub trait Deserialize: Sized {
+    fn deserialize(read: &mut impl Read) -> Result<Self>;
 }
 
 #[cfg(test)]
