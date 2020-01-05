6b7693c35dfb838df7d5e15cd170c12e8d1b1981
u1roh <terada.yuichiro+github@gmail.com>
2020-01-05 14:05:50 +0900

    rename test struct

--- src/lib.rs
+++ src/lib.rs
@@ -100,26 +100,26 @@ mod tests {
     use super::*;
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    struct Hoge {
+    struct DataVer0 {
         a: u32,
         b: u16,
     }
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    struct Hoge2 {
+    struct DataVer1 {
         a: u32,
         b: u16,
         c: u8,
     }
 
-    impl Serialize for Hoge {
+    impl Serialize for DataVer0 {
         fn serialize(&self, write: &mut (impl Write + ?Sized)) -> Result<()> {
             write.u32(self.a)?;
             write.u16(self.b)?;
             Ok(())
         }
     }
-    impl Serialize for Hoge2 {
+    impl Serialize for DataVer1 {
         const VERSION: u16 = 1;
         fn serialize(&self, write: &mut (impl Write + ?Sized)) -> Result<()> {
             write.u32(self.a)?;
@@ -128,15 +128,15 @@ mod tests {
             Ok(())
         }
     }
-    impl Deserialize for Hoge2 {
+    impl Deserialize for DataVer1 {
         fn deserialize(read: &mut (impl Read + ?Sized), version: u16) -> Result<Self> {
             match version {
-                0 => Ok(Self {
+                0 => Ok(DataVer1 {
                     a: read.u32()?,
                     b: read.u16()?,
                     c: 0,
                 }),
-                1 => Ok(Self {
+                1 => Ok(DataVer1 {
                     a: read.u32()?,
                     b: read.u16()?,
                     c: read.u8()?,
@@ -148,8 +148,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let x1 = Hoge { a: 123, b: 456 };
-        let x2 = Hoge2 {
+        let x1 = DataVer0 { a: 123, b: 456 };
+        let x2 = DataVer1 {
             a: 321,
             b: 654,
             c: 111,
@@ -157,14 +157,14 @@ mod tests {
         {
             let mut bin = Vec::<u8>::new();
             bin.obj(&x1).unwrap();
-            let y = bin.as_slice().obj::<Hoge2>().unwrap();
-            let y = Hoge { a: y.a, b: y.b };
+            let y = bin.as_slice().obj::<DataVer1>().unwrap();
+            let y = DataVer0 { a: y.a, b: y.b };
             assert_eq!(x1, y);
         }
         {
             let mut bin = Vec::<u8>::new();
             bin.obj(&x2).unwrap();
-            let y = bin.as_slice().obj::<Hoge2>().unwrap();
+            let y = bin.as_slice().obj::<DataVer1>().unwrap();
             assert_eq!(x2, y);
         }
     }
